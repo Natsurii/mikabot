3b01a2aa6a8b288a9fa4101687de1a3ae7e3f3aa
Natsurii <40201730+Natsurii@users.noreply.github.com>
2018-07-20 16:08:38 +0800

    cog update

--- main.py
+++ main.py
@@ -1,76 +1,86 @@
+#version 3.0
+#revised version of this bot framework
+#uses code for now on
 import discord
-import asyncio
-import aiohttp
 import os
-import time
+import aiohttp
+import asyncio
+import sys, traceback
 from discord.ext import commands
 from discord import Game
 
 #vars
-BOT_PREFIX = ['Nica ','nica ']
-bot = commands.Bot(command_prefix=BOT_PREFIX)
+version = '3.0 ExpNature'
+token = os.environ['TOKEN']
 
-@bot.event
-async def on_ready():
-	game = discord.Game("use Nica help | @vduterteee")
-	await bot.change_presence(status=discord.Status.idle, activity=game)
-	print('Hi bwoss! We have already contacted Discord as {0.user}'.format(bot))
+def get_prefix(bot, message):
 
-@bot.event
-async def on_message(message):
-	if message.author == bot.user:
-		return
+    prefixes = ['n^', 'N^']
+# Check to see if we are outside of a guild. e.g DM's etc.
+    if not message.guild:
+# Only allow ? to be used in DMs
+        return '?'
+# If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
+    return commands.when_mentioned_or(*prefixes)(bot, message)
 
-	if message.content.startswith('hello'):
-		msg = 'Hello {0.author.mention}'.format(message)
-		await message.channel.send(msg)
 
-	if message.content.startswith('owo'):
-		msg = '*notices* bulge'.format(message)
-		await message.channel.send(msg)
+bot = commands.Bot(command_prefix=get_prefix)
+initial_extensions = ['cogs.simple',
+                      'cogs.members',
+                      'cogs.owner']
 
-	if message.content.startswith('uwu'):
-		msg = 'uwu'.format(message)
-		await message.channel.send(msg)
-		
-	await bot.process_commands(message)
-		
-@bot.command()
-async def ping(ctx):
-	"""Ping latency"""
-	start = time.monotonic()
-	msg = await ctx.send(':ping_pong:Pong!')
-	millis = (time.monotonic() - start) * 1000
-	# Since sharded bots will have more than one latency, this will average them if needed.
-	heartbeat = ctx.bot.latency * 1000
-	embed=discord.Embed(title="Bot Latency", description="The bot received your latency request.", color=0xe7aeff) 
-	embed.set_thumbnail(url="https://emojipedia-us.s3.amazonaws.com/thumbs/320/apple/129/table-tennis-paddle-and-ball_1f3d3.png")
-	embed.add_field(name='ACK', value=str("%.2f" %millis) + ' ms', inline=False) 
-	embed.add_field(name='Heartbeat', value=str("%.2f" %heartbeat) +'ms', inline=True) 
-	embed.set_footer(text="Note: Latencies are different to other servers. ") 
-	await msg.edit(embed=embed)
-	
-@bot.command()
-async def copyme(ctx, *, content):
-    """Repeats a message multiple times."""
-    embed = discord.Embed(title='Im a copy-cat bot!', color=0x65ea15)
-    embed.add_field(name='You said...', value=content, inline=False)
-    await ctx.send(embed=embed)
-	
-def owner(ctx):
+
+# Here we load our extensions(cogs) listed above in [initial_extensions].
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+
+def is_owner(ctx):
     return ctx.message.author.id == 305998511894167552
     
 @bot.command()
-@commands.check(owner)
+@commands.check(is_owner)
 async def b(ctx, member):
     msg = await ctx.send('{} ***was banned***'.format(member))
     F = '\N{REGIONAL INDICATOR SYMBOL LETTER F}'
     await msg.add_reaction(F)
 
-@bot.command(name='eval')
-@commands.check(owner)
-async def _eval(ctx, *, code):
-    """A bad example of an eval command"""
-    await ctx.send(eval(code))
-	
-bot.run(os.environ['TOKEN'])
+
+@bot.event
+async def on_ready():
+    game = discord.Game("the n^help command!")
+    await bot.change_presence(status=discord.Status.idle, activity=game)    
+
+    print('==========================================================')
+    print('.##....##.####..######.....###...........................')
+    print('.###...##..##..##....##...##.##........................')
+    print('.####..##..##..##........##...##......................')
+    print('.##.##.##..##..##.......##.....##...................')
+    print('.##..####..##..##.......#########.................')
+    print('.##...###..##..##....##.##.....##................')
+    print(f'.##....##.####..######..##.....##..............  {version}')
+    print('=============================================')
+    
+    print(f'Veronica Bot ver.{version}')
+    print(f'Hi bwoss! We have already contacted Discord! We are inside of {bot.user}\'s account!')
+
+@bot.command()
+async def ping(ctx):
+    """Ping latency"""
+    start = time.monotonic()
+    msg = await ctx.send(':ping_pong:Pong!')
+    millis = (time.monotonic() - start) * 1000
+    # Since sharded bots will have more than one latency, this will average them if needed.
+    heartbeat = ctx.bot.latency * 1000
+    embed=discord.Embed(title="Bot Latency", description="The bot received your latency request.", color=0xe7aeff) 
+    embed.set_thumbnail(url="https://emojipedia-us.s3.amazonaws.com/thumbs/320/apple/129/table-tennis-paddle-and-ball_1f3d3.png")
+    embed.add_field(name='ACK', value=str("%.2f" %millis) + ' ms', inline=False) 
+    embed.add_field(name='Websocket', value=str("%.2f" %heartbeat) +'ms', inline=True) 
+    embed.set_footer(text="Note: Latencies are different to other servers. ") 
+    await msg.edit(embed=embed)
+bot.run(token, bot=True, reconnect=True)
