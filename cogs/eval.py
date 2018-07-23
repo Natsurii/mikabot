1b99ccc2abb9f20dedce1c0bbe549aec906e42ec
Natsurii <40201730+Natsurii@users.noreply.github.com>
2018-07-23 17:16:58 +0800

    Update eval.py

--- cogs/eval.py
+++ cogs/eval.py
@@ -18,6 +18,7 @@ class Eval():
             return '\n'.join(content.split('\n')[1:(-1)])  # Remove ```py\n```
 
     @commands.command(hidden=True, name='eval')
+    @commands.check (owner)
     async def _eval(self, ctx, *, body: str):
         env = {
             'bot': self.bot,
